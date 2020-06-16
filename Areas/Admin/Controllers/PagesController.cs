905ac12de821291db9ea5fb1fae7f4332768789d
Bobovskiy Konstantin <kbobovskiy@yandex.ru>
2020-06-16 20:45:06 +0300

    Add Admin area with page controller.

---
+++ Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShopingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PagesController : Controller
+    {
+        public string Index()
+        {
+            return "test string";
+        }
+    }
+}
