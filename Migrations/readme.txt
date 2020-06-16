5504fd84f39e7da2bf9a42e768332b35f657ae58
Bobovskiy Konstantin <kbobovskiy@yandex.ru>
2020-06-16 20:24:10 +0300

    Add SQL file data base

---
+++ Migrations/readme.txt
@@ -0,0 +1,8 @@
+﻿Создание БД:
+1 Команда:
+Add-Migration InitalCreate
+
+2 Команда:
+Update-database
+
+3 Просмотр базы через SQL server object explorer
--- Models/page.cs
+++ Models/page.cs
@@ -8,7 +8,6 @@ namespace CmsShopingCart.Models
 {
     public class Page
     {
-        [Required]
         public int Id { get; set; }
 
         [Required]
