c2abb9047da85995be3a53770f0021a44704697d
Bobovskiy Konstantin <kbobovskiy@yandex.ru>
2020-06-16 20:37:36 +0300

    Seed data into table Pages

---
+++ Models/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using CmsShopingCart.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace CmsShopingCart.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var contex = new CmsShopingCartContext(
+                serviceProvider.GetRequiredService<DbContextOptions<CmsShopingCartContext>>()))
+            {
+                if (contex.Pages.Any())
+                {
+                    return;
+                }
+
+                contex.Pages.AddRange(
+                    new Page
+                    {
+                        Title = "Home",
+                        Slug = "home",
+                        Content = "Home page",
+                        Sorting = 0
+                    },
+                    new Page
+                    {
+                        Title = "About Us",
+                        Slug = "about-us",
+                        Content = "about us page",
+                        Sorting = 100
+                    },
+                    new Page
+                    {
+                        Title = "Services",
+                        Slug = "services",
+                        Content = "services page",
+                        Sorting = 100
+                    },
+                    new Page
+                    {
+                        Title = "Contact",
+                        Slug = "contact",
+                        Content = "contact page",
+                        Sorting = 100
+                    }
+                    );
+                contex.SaveChanges();
+            }
+        }
+    }
+}
