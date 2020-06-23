28af50c219ff84c039d1ef246cb6e103384fafff
Bobovskiy Konstantin <kbobovskiy@yandex.ru>
2020-06-23 22:33:49 +0300

    Add Categories list page. Add Categories into menu nav

---
+++ Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShopingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShopingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly CmsShopingCartContext context;
+
+        public CategoriesController(CmsShopingCartContext context)
+        {
+            this.context = context;
+        }
+
+        //GET /admin/categories
+        public async Task<IActionResult> Index()
+        {
+            return View(await context.Categories.OrderBy(c => c.Sorting).ToListAsync());
+        }
+    }
+}
